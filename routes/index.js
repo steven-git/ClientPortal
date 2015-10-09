ca5c89528aa5bbc5f0a34ef2679605e2e85f9a14
Steven Garcia <steven.s.r.garcia@gmail.com>
2015-10-08 21:34:04 -0400

    Figured out basecamp api for info and incorporated it for all projects page

--- routes/index.js
+++ routes/index.js
@@ -51,53 +51,30 @@ router.get('/news', function(req, res, next) {
 				}
 			});
 
-			console.log(newsArray);
+			//console.log(newsArray);
 			res.json(newsArray); //newsArray sends all entry divs children
 		}
 	});
 });
 
 router.get('/projects', function(req, res, next) {
-	var username = new Buffer("steve@marketing2marketers.com").toString();
-	var password = new Buffer("dchampizhere2432").toString();
+var options = {
+	url: 'https://basecamp.com/2707120/api/v1/projects.json',
+	headers: {
+		'User-Agent': 'MyApp(steven@basecamp.com)'
+	},
+	auth:{
+		user: 'steve@marketing2marketers.com',
+		pass: 'dchampizhere2432'
+	}
+}
 
-	var options = {
-		url: 'https://basecamp.com',
-		port: 443,
-		path: '/2707120/',
-		//authentication headers
-		auth:{
-			user: username,
-			pass: password
-		}
-
-	};
-
-	request(options, function(err, res, html){
-		if(err){
-			console.log(err);
-			return
-		}
-
-		//console.log(html);
-		$ = cheerio.load(html);
-			//console.log($('feed').find($('entry')).length + 4); // All children of feed. currently 16
-
-			var entryArray = [];
-
-			$('body').filter(function(){
-
-				//for(var i = 5; i <= $('feed').find($('entry')).length + 4; i++){
-					entryArray.push({
-						'entry': $(this).html()
-					});	
-				//}
-	
-			});
-			console.log(entryArray);
-			//res.json(entryArray);
-	});
-
+	request(options, function(error, message, html){
+		if (!error) {
+			console.log(html);
+			res.json(html);
+		};
+	}).auth('steve@marketing2marketers.com', 'dchampizhere2432', false);
 
 });
 
