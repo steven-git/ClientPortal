7ded9ef77cfd928689684d6e92a3a3b7d3f6f1ff
Steven <steven.s.r.garcia@gmail.com>
2015-10-09 16:52:08 -0400

    Added code for api basecamp. pulling data from basecamp successfully

--- routes/index.js
+++ routes/index.js
@@ -9,29 +9,69 @@ router.get('/', function(req, res, next){
 });
 
 router.get('/feed', function(req, res, next) {
-	request.get('https://basecamp.com/2707120/projects/10611786.atom', function(error, response, body){
-		if(!error){
-			//console.log(body);
-			$ = cheerio.load(body);
-			//console.log($('feed').find($('entry')).length + 4); // All children of feed. currently 16
+var options = {
+	url: 'https://basecamp.com/2707120/api/v1/events.json',
+	headers: {
+		'User-Agent': 'MyApp(steven@basecamp.com)'
+	},
+	auth:{
+		user: 'steve@marketing2marketers.com',
+		pass: 'dchampizhere2432'
+	}
+};
 
-			var entryArray = [];
-
-			$('feed').filter(function(){
-
-				for(var i = 5; i <= $('feed').find($('entry')).length + 4; i++){
-					entryArray.push({
-						'entry': $(this).children().eq(i).html()
-					});	
-				}
-	
-			});
-
-			//console.log(entryArray);
-			res.json(entryArray); //entryArray sends all entry divs children
-		}
+	request(options, function(error, message, html){
+		if (!error) {
+			console.log(html);
+			res.json(html);
+		};
 	}).auth('steve@marketing2marketers.com', 'dchampizhere2432', false);
+
 });
+
+router.get('/projects', function(req, res, next) {
+var options = {
+	url: 'https://basecamp.com/2707120/api/v1/projects.json',
+	headers: {
+		'User-Agent': 'MyApp(steven@basecamp.com)'
+	},
+	auth:{
+		user: 'steve@marketing2marketers.com',
+		pass: 'dchampizhere2432'
+	}
+};
+
+	request(options, function(error, message, html){
+		if (!error) {
+			//console.log(html);
+			res.json(html);
+		};
+	}).auth('steve@marketing2marketers.com', 'dchampizhere2432', false);
+
+});
+
+
+router.get('/calendar', function(req, res, next) {
+var options = {
+	url: 'https://basecamp.com/2707120/api/v1/calendars/1444500/calendar_events.json',
+	headers: {
+		'User-Agent': 'MyApp(steven@basecamp.com)'
+	},
+	auth:{
+		user: 'steve@marketing2marketers.com',
+		pass: 'dchampizhere2432'
+	}
+};
+
+	request(options, function(error, message, html){
+		if (!error) {
+			console.log(html);
+			res.json(html);
+		};
+	}).auth('steve@marketing2marketers.com', 'dchampizhere2432', false);
+
+});
+
 
 router.get('/news', function(req, res, next) {
 	request.get('http://marketing2marketers.curatasite.com/articles/featured/', function(error, response, body){
@@ -56,28 +96,6 @@ router.get('/news', function(req, res, next) {
 		}
 	});
 });
-
-router.get('/projects', function(req, res, next) {
-var options = {
-	url: 'https://basecamp.com/2707120/api/v1/projects.json',
-	headers: {
-		'User-Agent': 'MyApp(steven@basecamp.com)'
-	},
-	auth:{
-		user: 'steve@marketing2marketers.com',
-		pass: 'dchampizhere2432'
-	}
-}
-
-	request(options, function(error, message, html){
-		if (!error) {
-			console.log(html);
-			res.json(html);
-		};
-	}).auth('steve@marketing2marketers.com', 'dchampizhere2432', false);
-
-});
-
 
 
 module.exports = router;
